06fe6a0c186436c0f5d5a352acea0e212a7a6668
User <User@DESKTOP-SDBFMO6>
2021-12-17 12:32:56 +0200

    Add project files.

---
+++ Lab5z2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2
+{
+    interface IPrintable
+    {
+        void print();
+    }
+    class Book : IPrintable
+    {
+        public void print()
+        {
+            Console.WriteLine("Книга");
+        }
+    }
+    class Magazine : IPrintable
+    {
+        public void print()
+        {
+            Console.WriteLine("Журнал");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPrintable[] bm = new IPrintable[6];
+            bm[0] = new Book();
+            bm[1] = new Magazine();
+            bm[2] = new Book();
+            bm[3] = new Magazine();
+            bm[4] = new Book();
+            bm[5] = new Magazine();
+            foreach (var printable in bm)
+            {
+                printable.print();
+            }
+        }
+    }
+}
